293bd6e75a32077b85b138fbffe6e16d394df31f
Farhat Gull Raina <53214104+farhat17@users.noreply.github.com>
2025-08-21 10:55:01 +0530

    Update build.sh

--- converter-master/build.sh
+++ converter-master/build.sh
@@ -1,1 +1,13 @@
 
+set -o errexit
+pip install -r requirement.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+    python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_NAME" --email "$DJANGO_SUPERUSER_EMAIL"
+    echo "Superuser created with username: $DJANGO_SUPERUSER_NAME and email: $DJANGO_SUPERUSER_EMAIL"
+else
+    echo "Superuser creation skipped."
+fi
+python manage.py runserver  
+
